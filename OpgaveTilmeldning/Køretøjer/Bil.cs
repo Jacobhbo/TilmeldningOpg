f7a6d45be27d52dac1a61612d8b1b429e43b3c6e
Tec <Tec@SKAB2-PC7>
2023-08-24 10:10:03 +0100

    OOPExercises Køretøj

---
+++ OpgaveTilmeldning/Køretøjer/Bil.cs
@@ -0,0 +1,20 @@
+﻿using OpgaveTilmeldning.Køretøjer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TecTilmeldning.Køretøjer
+{
+    internal sealed class Bil : Kørtøj
+    {
+        public double MotorStørrelse { get; set; }
+
+
+        public Bil(string? mærke, string? model, double motorStørrelse) : base(mærke, model)
+        {
+            MotorStørrelse = motorStørrelse;
+        }
+    }
+}
---
+++ OpgaveTilmeldning/Køretøjer/Kørtøj.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TecTilmeldning.Køretøjer
+{
+    internal class Kørtøj
+    {
+        public string? Mærke { get; set; }
+        public string? Model { get; set; }
+
+
+        public Kørtøj(string? mærke, string? model)
+        {
+            Mærke = mærke;
+            Model = model;
+
+        }
+    }
+}
